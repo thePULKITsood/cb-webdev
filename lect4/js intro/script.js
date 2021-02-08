e991b5a8f1079b1bc0dba64c9eec4fd85e7f8325
thePULKITsood <the.pulit.sood@gmail.com>
2021-02-08 13:20:16 +0530

    commit

--- lect4/js intro/script.js
+++ lect4/js intro/script.js
@@ -1,2 +1,36 @@
 console.log('external script')
 alert('ext src')
+
+
+
+"strongly typed language   VS     weekly typed language "
+typed means the data type  ! ! 
+
+
+
+
+// L-value: “l-value” refers to memory location which identifies an object.
+//  l-value may appear as either left hand or right hand side of an assignment operator(=)
+//  . l-value often represents as identifier.
+
+// An lvalue (locator value) represents an object that occupies
+//  some identifiable location in memory (i.e. has an address).
+
+// rvalues are defined by exclusion. 
+// Every expression is either an lvalue or an rvalue, so
+// , an rvalue is an expression that does not 
+// represent an object occupying some identifiable location in memory.
+
+
+//    the restrictionn of data type is on the variable and not on the value
+
+
+//    ins strongly typed languages the l value has data types as well 
+//     means the poitner to the location in the memory also have data types 
+
+//      but in weekly typed languages the pointers or the le values dont have a a data type 
+
+
+
+
+// statically vs dynamically typed laguages ! ! 
